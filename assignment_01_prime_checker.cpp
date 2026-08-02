bc9fd9cd85295b3d58a99baea09f0ca7854b45d6
KingTJedOTum <ktjotum@st.ug.edu.gh>
2026-08-02 15:00:44 +0000

    Implement isPrime function logic

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,20 @@
 #include <iostream>
 using namespace std;
 
+bool isPrime(int n) {
+    if (n < 2) {
+        return false;
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    return 0;
+}
